4d82e47ec949a3df897a9c5ed77550295230f69d
Evance <155881420+EvanceK@users.noreply.github.com>
2024-09-13 09:25:21 +0800

    updatedb

--- evancebookstore_member.sql
+++ evancebookstore_member.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `evancebookstore` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `evancebookstore`;
 -- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: evancebookstore
@@ -56,4 +58,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-09-13  0:07:31
+-- Dump completed on 2024-09-13  9:23:56
